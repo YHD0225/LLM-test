93cbf7485ede7e7ab28b5794a73658bd067aa42e
YueHD <97381025+YHD0225@users.noreply.github.com>
2023-12-15 16:07:40 +0800

    Update llm_deeplearn.py

--- llm_deeplearn.py
+++ llm_deeplearn.py
@@ -1,7 +1,9 @@
 import os
 import openai
 
-openai.api_key = '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'
+# openai.api_key = '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'
+openai.api_key = 'sk-gLYd9kQMK0EOqmwjRojOT3BlbkFJc3hesLf8mT6AuI0'
+
 # client = OpenAI(api_key='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')
 # account for deprecation of LLM model
 import datetime
@@ -27,3 +29,32 @@ def get_completion(prompt, model=llm_model):
     return response.choices[0].message["content"]
 
 print(get_completion("What is 1+1?"))
+
+customer_email = """
+Arrr, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse,\
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+
+style = """American English \
+in a calm and respectful tone
+"""
+
+prompt = f"""Translate the text \
+that is delimited by triple backticks
+into a style that is {style}.
+text: ```{customer_email}```
+"""
+
+# print(prompt)
+response = get_completion(prompt)
+print(response)
+
+prompt = '请介绍一下蔚来汽车公司，具体详细'
+response = get_completion(prompt)
+print(response)
+
+
